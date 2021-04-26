80da97e37746dba772e0084beaa1e64f18f10e59
amoeba17 <be.nies17@protonmail.com>
2021-04-25 23:54:42 -0400

    Implement the game algorithm into an event listener, make gameplay more interactive, add images.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+
+// Constants
 const choices = ['Rock', 'Paper', 'Scissors']; 
 
+
+// Game functions
 function computerPlay() {
     return Math.floor(Math.random() * choices.length);
 }
 
 function playRound(playerSelection, computerSelection) { // both integers 0 (rock), 1 (paper), or 2 (scissors)
-    console.log('Player selection: ' + choices[playerSelection]);
-    console.log('Computer selection: '+ choices[computerSelection]);
+
+    console.log('Player selection: ' + choices[playerSelection] + ' | Computer selection: '+ choices[computerSelection]);
 
     if (playerSelection == computerSelection) {
         return 0; // tie
@@ -18,42 +22,64 @@ function playRound(playerSelection, computerSelection) { // both integers 0 (roc
     throw Error('unreachable line of code');
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let input;
-        do {
-            input = prompt('Rock | Paper | Scissors: ').toLowerCase();
-        } while (input != 'rock' && input != 'paper' && input != 'scissors');
 
-        let playerSelection;
-        if (input == 'rock') {
-            playerSelection = 0;
-        } else if (input == 'paper') {
-            playerSelection = 1;
-        } else {
-            playerSelection = 2;
-        }
+// DOM functions
+function dialogue(message) {
+    const dialogueDiv = document.querySelector('#dialogue');
+    dialogueDiv.textContent = message;
+
+}
+
+function setScore(playerScore, computerScore) {
+    const scoreboard = document.querySelector('#scoreboard')
+    scoreboard.textContent = `Player ${playerScore} | Computer ${computerScore}`;
+}
+
+function setImageDivs(playerSelection, computerSelection) {
+    const playerImg = document.querySelector('#player-img');
+    playerImg.src = `${choices[playerSelection]}.png`;
+
+    const computerImg = document.querySelector('#computer-img');
+    computerImg.src = `${choices[computerSelection]}.png`;
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.getAttribute('id');
         let computerSelection = computerPlay();
-        
+        setImageDivs(playerSelection, computerSelection);
         let result = playRound(playerSelection, computerSelection);
         if (result == 1) {
-            alert(`You win! ${choices[playerSelection]} beats ${choices[computerSelection]}.`);
-            playerScore++;
+            dialogue(`You win! ${choices[playerSelection]} beats ${choices[computerSelection]}.`);
+            setScore(++playerScore, computerScore);
             if (playerScore >= 3) {
-                return 'You won the game!';
+                dialogue('You won the game!');
+                console.clear()
+                playerScore = 0;
+                computerScore = 0;
+                setScore(playerScore, computerScore);
             }
         } else if (result == -1) {
-            alert(`You lose! ${choices[computerSelection]} beats ${choices[playerSelection]}.`);
-            computerScore++;
+            dialogue(`You lose! ${choices[computerSelection]} beats ${choices[playerSelection]}.`);
+            setScore(playerScore, ++computerScore);
             if (computerScore >= 3) {
-                return 'You lost the game :{';
+                dialogue('You lost the game :{');
+                console.clear()
+                playerScore = 0;
+                computerScore = 0;
+                setScore(playerScore, computerScore);
             }
         } else { // result == 0
-            alert("It's a tie!");
-            i--;
+            dialogue("It's a tie!");
         }
-    }
-    return 'unexpected return';
-}
+
+    });
+});
+
+
+
+//variables
+let playerScore = 0;
+let computerScore = 0;
+setScore(playerScore, computerScore)
